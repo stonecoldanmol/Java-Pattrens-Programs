320a399c1f322ae9c090779c8a61a3654156a2b7
Anmol Sinha <54975830+stonecoldanmol@users.noreply.github.com>
2019-09-28 09:22:06 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,24 @@
+class Patt4
+{
+public static void main(String[] args)
+{
+  int i, j, num=1; 
+          
+    
+        for(i=0; i<4; i++) 
+        { 
+  
+            
+            for(j=0; j<=i; j++) 
+            { 
+               
+                System.out.print(num+ " "); 
+                  
+              
+                num = num + 1; 
+            } 
+  
+            System.out.println(); 
+        } 
+    } 
+}
