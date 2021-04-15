bb2635827ad461d32479f8325ae14def0ea05fe9
Anmol Sinha <54975830+stonecoldanmol@users.noreply.github.com>
2021-04-15 18:10:23 +0530

    Add files via upload

---
+++ NumberPattern4.java
@@ -0,0 +1,17 @@
+class NumberPattern4
+{
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=5;i++)	
+		{
+			for(int j=i;j>=1;j--)
+			{
+				System.out.print(j+" ");
+			}
+		System.out.println();
+		}
+	}
+
+
+
+}
